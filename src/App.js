e37758ee03b0eff177d5c8e3c37b0b2484884151
Harsh Lakhani <20it061@charusat.edu.in>
2022-09-07 19:57:41 +0530

    Added Students List

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Footer from "./Components/Footer";
 import NavBar from "./Components/NavBar";
 import Student from "./Components/Student";
 import StudentList from "./Components/StudentList";
-
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
 
       <NavBar />
+           <Container>
            <Student />
            <StudentList />
+           </Container>
            <Footer />
 
     </div>
--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,42 +1,53 @@
-import React from "react";
-import {Card, Container, Table } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Card, Container, Table} from "react-bootstrap";
+import axios from "axios";
 
 export default function StudentList() {
-  return (
-    <div className="my-3">
-      <Container>
-        <Card.Header>
-                  <h3>Student List</h3>
-                </Card.Header>
-                <Card.Body>
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>Student Id</th>
-                        <th>Student Name</th>
-                        <th>Student Address</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </Card.Body>
-      </Container>
-    </div>
-  );
+  const [students, setStudents] = useState([]);
+
+      useEffect(() => {
+          getStudents();
+      }, [])
+
+      let getStudents = () => {
+          axios.get("http://localhost:8080/listStudents")
+              .then((response) => setStudents(response.data))
+              .catch((error) => alert(error));
+      }
+      return (
+          <div className="my-3">
+              <Container>
+                  <Card.Header>
+                      <h3>Student List</h3>
+                  </Card.Header>
+                  <Card.Body>
+                      <Table striped bordered hover>
+                          <thead>
+                          <tr>
+                              <th>Student Id</th>
+                              <th>Student Name</th>
+                              <th>Student Address</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          {
+                              students.length === 0 ? (
+                                      <tr>
+                                          <td>{students.length} Students Available!!!</td>
+                                      </tr>
+                                  ) :
+                                  (students.map((student) =>
+                                      <tr>
+                                          <td>{student.id}</td>
+                                          <td>{student.name}</td>
+                                          <td>{student.address}</td>
+                                      </tr>
+                                  ))
+                          }
+                          </tbody>
+                      </Table>
+                  </Card.Body>
+              </Container>
+          </div>
+      );
 }
