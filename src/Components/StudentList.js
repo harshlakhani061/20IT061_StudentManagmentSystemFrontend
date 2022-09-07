3e39da4499d2f8196ffc0ff3ceba61705045aee7
Harsh Lakhani <20it061@charusat.edu.in>
2022-09-07 19:29:54 +0530

    Implement Add Student

--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,36 +1,41 @@
 import React from "react";
-import { Container, Table } from "react-bootstrap";
+import {Card, Container, Table } from "react-bootstrap";
 
 export default function StudentList() {
   return (
     <div className="my-3">
       <Container>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Student Id</th>
-              <th>Student Name</th>
-              <th>Student Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
+        <Card.Header>
+                  <h3>Student List</h3>
+                </Card.Header>
+                <Card.Body>
+                  <Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <th>Student Id</th>
+                        <th>Student Name</th>
+                        <th>Student Address</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Card.Body>
       </Container>
     </div>
   );
